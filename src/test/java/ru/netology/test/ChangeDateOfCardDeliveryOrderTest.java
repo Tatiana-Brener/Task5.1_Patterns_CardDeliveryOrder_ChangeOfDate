14c9cdcb04d0a0a20e6d607af2def8e1b34bd6d3
Tatana <burmatova@inbox.ru>
2021-03-03 20:55:16 +0300

    fixed tests

--- src/test/java/ru/netology/test/ChangeDateOfCardDeliveryOrderTest.java
+++ src/test/java/ru/netology/test/ChangeDateOfCardDeliveryOrderTest.java
@@ -14,26 +14,46 @@ import static com.codeborne.selenide.Selenide.*;
 public class ChangeDateOfCardDeliveryOrderTest {
 
     @Test
-    void shouldSendCardDeliveryForm () {
+    void shouldSendCardDeliveryFormWhenCityIsValid () {
         DataForRegistration newUser = DataGenerator.Registration.generateByCard("ru");
+        String validCity = DataGenerator.Registration.generateValidCity();
         open("http://localhost:9999");
-
-        $("[data-test-id=city] [placeholder=Город]").setValue(newUser.getCity());
+        $("[data-test-id=city] [placeholder=Город]").setValue(validCity);
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[placeholder='Дата встречи']").setValue(newUser.getDataOfMeeting());
         $("[data-test-id='name'] .input__control").setValue(newUser.getName());
         $("[data-test-id='phone'] .input__control").setValue(newUser.getPhone());
         $("[data-test-id='agreement'] .checkbox__box").click();
         $$("[type='button']").find(exactText("Запланировать")).click();
+        $("[data-test-id=success-notification] .notification__content").
+                shouldBe(Condition.visible, Duration.ofMillis(15000)).
+                shouldHave(text("Встреча успешно запланирована на " + newUser.getDataOfMeeting()));
 
-        if(($(".notification_visible[data-test-id=success-notification] .notification__content")).isDisplayed()) {
-            ($("[data-test-id=success-notification] .notification__content")).
-                    shouldBe(Condition.visible, Duration.ofMillis(15000)).
-                    shouldHave(text("Встреча успешно забронирована на " + newUser.getDataOfMeeting()));
-        } else {
-            $(".input_invalid[data-test-id=city] .input__sub").
-                    shouldHave(exactText("Доставка в выбранный город недоступна"));
-        }
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(newUser.getAnotherDataOfMeeting());
+        $$("[type='button']").find(exactText("Запланировать")).click();
+        $("[data-test-id=replan-notification] .notification__content").
+                shouldBe(Condition.visible, Duration.ofMillis(15000)).
+                shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $$(".button__text").find(exactText("Перепланировать")).click();
+        $("[data-test-id=success-notification] .notification__content").
+                shouldBe(Condition.visible, Duration.ofMillis(15000)).
+                shouldHave(text("Встреча успешно запланирована на " + newUser.getAnotherDataOfMeeting()));
     }
 
+    @Test
+    void shouldSendCardDeliveryFormWhenCityIsInvalid () {
+        DataForRegistration newUser = DataGenerator.Registration.generateByCard("ru");
+        String invalidCity = DataGenerator.Registration.generateInvalidCity();
+        open("http://localhost:9999");
+        $("[data-test-id=city] [placeholder=Город]").setValue(invalidCity);
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(newUser.getDataOfMeeting());
+        $("[data-test-id='name'] .input__control").setValue(newUser.getName());
+        $("[data-test-id='phone'] .input__control").setValue(newUser.getPhone());
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $$("[type='button']").find(exactText("Запланировать")).click();
+        $(".input_invalid[data-test-id=city] .input__sub").
+                shouldHave(exactText("Доставка в выбранный город недоступна"));
+    }
 }
