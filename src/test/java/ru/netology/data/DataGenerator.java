074ae392f9d5279328203eb8af4a92cdc149dcb3
Tatana <burmatova@inbox.ru>
2021-03-03 20:55:01 +0300

    added methods for generate cities

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -4,9 +4,12 @@ import com.github.javafaker.Faker;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.Random;
 
 public class DataGenerator {
+
     public DataGenerator() {}
 
     public static class Registration {
@@ -15,13 +18,27 @@ public class DataGenerator {
         public static DataForRegistration generateByCard(String local) {
             Faker faker = new Faker(new Locale("ru"));
             return new DataForRegistration(
-                    faker.address().cityName(),
                     LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
                     LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
                     faker.name().fullName(),
                     faker.phoneNumber().phoneNumber()
             );
         }
+
+        public static String generateValidCity() {
+            String[] validCity = {"Москва", "Тула", "Краснодар", "Казань", "Воронеж"};
+            Random random = new Random();
+            int randomIndex = random.nextInt(validCity.length);
+            return validCity[randomIndex];
+        }
+
+        public static String generateInvalidCity() {
+            String[] invalidCity = {"Сочи", "Новороссийск", "Орел", "Павловск"};
+            Random random = new Random();
+            int randomIndex = random.nextInt(invalidCity.length);
+            return invalidCity[randomIndex];
+        }
+
     }
 
 }
